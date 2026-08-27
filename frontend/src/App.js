dca6b608f31e5f9f41c968219b89411c202cb193
meowliodaz <trieuhaipham32621998@gmail.com>
2026-08-27 16:37:29 +1000

    SCRUM-26 Initial setup: index.js, App.js, etc.

---
+++ frontend/src/App.js
@@ -0,0 +1,29 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Profile from './pages/Profile';
+import Home from './pages/Home';
+import Game from './pages/Game';
+import Admin from './pages/Admin';
+import UserManagement from './pages/UserManagement';
+
+function App() {
+  return (
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/home" element={<Home />} />
+        <Route path="/game/:id" element={<Game />} />
+        <Route path="/admin" element={<Admin />} />
+        <Route path="/user-management" element={<UserManagement />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
