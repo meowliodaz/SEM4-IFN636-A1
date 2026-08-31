e9b7c499d4a58e6100dde3ef326119195a0a9687
meowliodaz <trieuhaipham32621998@gmail.com>
2026-08-31 20:27:43 +1000

    SCRUM-38 Basic routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Home from './pages/Home';
 import Game from './pages/Game';
+import Review from './pages/Review';
 import Admin from './pages/Admin';
 import UserManagement from './pages/UserManagement';
 
@@ -19,6 +20,7 @@ function App() {
         <Route path="/profile" element={<Profile />} />
         <Route path="/home" element={<Home />} />
         <Route path="/game/:id" element={<Game />} />
+        <Route path="/game/:id/your-review" element={<Review />} />
         <Route path="/admin" element={<Admin />} />
         <Route path="/user-management" element={<UserManagement />} />
       </Routes>
