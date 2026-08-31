631def24591bca8d87aa7f5ff83cd8c6fe61d299
meowliodaz <trieuhaipham32621998@gmail.com>
2026-08-31 20:53:22 +1000

    SCRUM-53 Backend: User CRUD, update

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,12 +10,19 @@ const generateToken = (id) => {
 // Register - Create user
 const registerUser = async (req, res) => {
 	const { name, email, password } = req.body;
+	const permission = req.body.permission ? req.body.permission : "user";
 	try {
 		const userExists = await User.findOne({ email });
 		if (userExists) return res.status(400).json({ message: 'User already exists' });
 
 		const user = await User.create({ name, email, password });
-		res.status(201).json({ id: user.id, name: user.name, email: user.email, token: generateToken(user.id) });
+		res.status(201).json({
+			id: user.id,
+			name: user.name,
+			email: user.email,
+			token: generateToken(user.id),
+			permission: permission
+		});
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
@@ -121,9 +128,18 @@ const updateUser = async (req, res) => {
 		user.email = email || user.email;
 		user.university = university || user.university;
 		user.address = address || user.address;
+		user.permission = permission || user.permission;
 
 		const updatedUser = await user.save();
-		res.json({ id: updatedUser.id, name: updatedUser.name, email: updatedUser.email, university: updatedUser.university, address: updatedUser.address, token: generateToken(updatedUser.id) });
+		res.json({
+			id: updatedUser.id,
+			name: updatedUser.name,
+			email: updatedUser.email,
+			university: updatedUser.university,
+			address: updatedUser.address,
+			permission: updatedUser.permission,
+			token: generateToken(updatedUser.id)
+		});
 	} catch (error) {
 		res.status(500).json({ message: error.message });
 	}
