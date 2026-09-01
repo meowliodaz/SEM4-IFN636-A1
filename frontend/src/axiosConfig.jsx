6f47114c1b061f4a87f5407b7f620038109b8de5
meowliodaz <trieuhaipham32621998@gmail.com>
2026-09-01 10:37:28 +1000

    Change axios URL to connect with backend (axiosConfig.jsx - axiosInstance)

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,12 @@
 import axios from 'axios';
 
+// NOTE:  This reset everytime the EC2 instance reset.
+//        Needs to be changed again on demo day
+const EC2_URL = "52.65.8.175";
+
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5001', // local
-  //baseURL: 'http://3.26.96.188:5001', // live
+  // baseURL: 'http://localhost:5001', // local
+  baseURL: `http://${EC2_URL}:5001`, // live
   headers: { 'Content-Type': 'application/json' },
 });
 
