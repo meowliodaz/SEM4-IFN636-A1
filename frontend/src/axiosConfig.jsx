d4b89f31b0d2c27c94ccff300ab922fb10944ec3
meowliodaz <trieuhaipham32621998@gmail.com>
2026-09-01 13:00:57 +1000

    Add env to frontend for easy "EC2 IP change"

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -1,8 +1,9 @@
 import axios from 'axios';
 
 // NOTE:  This reset everytime the EC2 instance reset.
-//        Needs to be changed again on demo day
-const EC2_URL = "52.65.8.175";
+//        Needs to be changed again on demo day.
+//        Changes can be made to .env file
+const EC2_URL = process.env.REACT_APP_EC2_URL;
 
 const axiosInstance = axios.create({
   // baseURL: 'http://localhost:5001', // local
